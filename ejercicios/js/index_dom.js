c23df58a0b75a94f6245162fa24396367599fdb0
rglz21 <68825014+rglz21@users.noreply.github.com>
2023-10-29 02:24:11 +0000

    Ejercicio de alarma terminado

--- ejercicios/js/index_dom.js
+++ ejercicios/js/index_dom.js
@@ -1,8 +1,11 @@
 import menu_hamburguesa from "./dom/menu_hamburguesa.js";
+import { digitalClock,alarm } from "./dom/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     menu_hamburguesa(".panel-btn",".panel",".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
 });
 
