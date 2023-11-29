dfcf0e7c1c94fba98c4ae97e8fc333dbc62a7ed0
rglz21 <68825014+rglz21@users.noreply.github.com>
2023-11-29 23:46:04 +0000

    Terminado scroll

--- ejercicios/js/index_dom.js
+++ ejercicios/js/index_dom.js
@@ -2,6 +2,7 @@ import menu_hamburguesa from "./dom/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./dom/reloj.js";
 import { moveBall,shortcuts } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js"
+import scrollTopButton from "./dom/boton_scroll.js";
 
 const d = document;
 
@@ -10,6 +11,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
     countdown("countdown","Nov 14,2023 17:15:19","Feliz cumpleaños amigo.");
+    scrollTopButton(".scroll-stop-btn");
 });
 
 d.addEventListener("keydown",e=>{
